73b6c44296f828a3654b5212b7ed404c37f3cfd9
wupeng <2113168917@qq.com>
2021-12-27 20:39:10 +0800

    second commit

--- src/main/java/com/atguigu/beans/Customer.java
+++ src/main/java/com/atguigu/beans/Customer.java
@@ -4,5 +4,6 @@ public class Customer {
     public static void main(String[] args) {
         System.out.println("张山");
         System.out.println("张山");
+        System.out.println("张山");
     }
 }
